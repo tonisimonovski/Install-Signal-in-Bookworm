9992989e67964075593850d58a82c00c32182a70
tonisimonovski <149823950+tonisimonovski@users.noreply.github.com>
2024-04-04 13:15:43 +0200

    Add files via upload

---
+++ install_signal_in_Bookworm.sh
@@ -0,0 +1,13 @@
+# NOTE: 64-bit Debian-based
+# Linux distributions based on Debian such as Ubuntu, Mint etc.
+
+# Install official public software signing key:
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# Update pkgc dbase and install Signal:
+sudo apt update -y 
+sudo apt install signal-desktop
